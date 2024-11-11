ab43fb672ba3b3ebd07d947f3655ee2d8df2120b
zack6yuan <zack6yuan@gmail.com>
2024-11-10 23:18:56 -0600

    changes.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,37 +14,40 @@ def employee_statistics(employee_id):
     Returns:
         - Employee data (complete + incomplete tasks)
     """
-    if __name__ == "__main__":
-        if not isinstance(employee_id, int):
-            raise TypeError("Employee ID must be an integer.")
-        elif len(argv) < 2:
-            raise ValueError("Please enter Employee ID.")
-        employee_id = int(argv[1])
+    if not isinstance(employee_id, int):
+        raise TypeError("Employee ID must be an integer.")
+    elif len(argv) < 2:
+        raise ValueError("Please enter Employee ID.")
+    employee_id = int(argv[1])
 
-        base = "https://jsonplaceholder.typicode.com"
+    base = "https://jsonplaceholder.typicode.com"
 
-        user = "{}/users/{}".format(base)
-        user_response = requests.get(user)
-        if user_response.status_code == 200:
-            username = user_response.json()
+    user = "{}/users/{}".format(base)
+    user_response = requests.get(user)
+    if user_response.status_code == 200:
+        username = user_response.json()
+    else:
+        return
+
+    todo = "{}/todos".format(base)
+    todo_response = requests.get(todo)
+    if todo_response.status_code == 200:
+        todo_total = todo_response.json()
+    else:
+        return
+
+    complete = 0
+    incomplete = 0
+    for todo in todo_total:
+        if todo["completed"]:
+            complete += 1
         else:
-            return
+            incomplete += 1
 
-        todo = "{}/todos".format(base)
-        todo_response = requests.get(todo)
-        if todo_response.status_code == 200:
-            todo_total = todo_response.json()
-        else:
-            return
+    print("Employee {} is done with tasks({}/{}):".format(
+        username, complete, incomplete
+    ))
 
-        complete = 0
-        incomplete = 0
-        for todo in todo_total:
-            if todo["completed"]:
-                complete += 1
-            else:
-                incomplete += 1
 
-        print("Employee {} is done with tasks({}/{}):".format(
-            username, complete, incomplete
-        ))
+if __name__ == "__main__":
+    employee_statistics(employee_id)
