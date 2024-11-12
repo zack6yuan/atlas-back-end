a078d55da43ee1c9aaa8741de20b4e8a44873b56
zack6yuan <zack6yuan@gmail.com>
2024-11-11 21:45:25 -0600

    update task 1.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -32,9 +32,9 @@ def employee_statistics(employee_id: int):
         for todo in todos_data:
             if todo['completed'] is True:
                 complete_tasks += 1
-
-    csv_convert = "{}.csv".format(employee_id)
+                
     employee_id = int(argv[1])
+    csv_convert = "{}.csv".format(employee_id)
     with open(csv_convert, mode='w', newline=' ') as temp:
         writer = csv.writer(temp, quoting=csv.QUOTE_ALL)
         csv_data = [employee_id, employee_name, complete_tasks, total_tasks]
